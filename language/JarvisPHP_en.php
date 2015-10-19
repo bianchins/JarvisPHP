7a42d6a40026b3e37070954f22059161a7caf1e9
bianchins <bianchins@gmail.com>
2015-10-19 12:32:56 +0200

    Bug fix

--- language/JarvisPHP_en.php
+++ language/JarvisPHP_en.php
@@ -1,4 +1,4 @@
 <?php
 $_lang['core_command_not_understand'] = 'I am sorry, Sir. I did not understand your command';
 $_lang['preg_match_magic_words_to_stop_session'] = '/ok/';
-$_lang['response_to_magic_words_to_stop_session'] ) 'I am at your service, Sir.';
+$_lang['response_to_magic_words_to_stop_session'] = 'I am at your service, Sir.';

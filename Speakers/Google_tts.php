4f07c1355659d005432a4b2d6a39450b9163f97f
bianchins <bianchins@gmail.com>
2015-11-12 15:12:02 +0100

    Bug fix Google TTS

--- Speakers/Google_tts.php
+++ Speakers/Google_tts.php
@@ -11,8 +11,22 @@ namespace JarvisPHP\Speakers;
 class Google_tts {
     
     public static function speak($sentence) {
-        file_put_contents('tts.mp3', file_get_contents('http://translate.google.com/translate_tts?tl='._LANGUAGE.'&q='.urlencode($sentence)));
+
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, array(
+		    CURLOPT_RETURNTRANSFER => 1,
+		    CURLOPT_URL => 'http://translate.google.com/translate_tts?ie=UTF-8&client=t&tl='._LANGUAGE.'&q='.urlencode($sentence),
+		    CURLOPT_USERAGENT => 'stagefright/1.2 (Linux;Android 5.0)',
+		    CURLOPT_REFERER => 'http://translate.google.com/'
+		));
+
+		$resp = curl_exec($curl);
+
+		curl_close($curl);
+
+        file_put_contents('tts.mp3', $resp);
         exec('aplay tts.mp3 && rm tts.mp3');
     }
-    
+
 }
